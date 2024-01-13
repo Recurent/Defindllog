984465ee3d4accf191fc0471a211adeb6476524f
Recurent <versus56rus@mail.ru>
2024-01-13 19:39:11 +0500

    Test

---
+++ first.go
@@ -0,0 +1,45 @@
+package main
+
+/*var name, surname = "Ivan", " Ivanov"
+var pizza = ", пицка вкусная была?"
+
+func main() {
+	nn, pp := Name(name, surname, pizza)
+	fmt.Printf("Hi %q %s", nn, pp)
+	fmt.Printf("Hi %q, ", Name(name, surname, pizka))
+}
+
+/*func Name(name, surname string) {
+	fmt.Printf("Hi %s %s\n", name, surname)
+}
+
+
+/*func Name() string {
+	return name + surname
+}
+
+
+func Name(name, surname, pizza string) (sum, pizka string) {
+	sum = name + surname
+	pizka = pizza
+	return sum, pizka
+}*/
+
+/*
+func main() {
+	a, b, c := 4, 6, 2
+	xd := owo(a, b, c, uwu)
+	fmt.Println(xd())
+}
+
+func uwu(a, b int) int {
+	return a + b
+}
+
+func owo(a, b, c int, hui func(a, b int) int) func() bool {
+	divider := func() bool {
+		return hui(a, b)%c == 0
+	}
+	return divider
+}
+*/
